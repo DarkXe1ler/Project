f1564034d840fd664f9fe73c545632b99e2dbfeb
vladimir <vladmoz44@gmail.com>
2023-03-16 20:54:42 +0300

    class_10

--- ClassLibrary1/Class10.cs
+++ ClassLibrary1/Class10.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Class10
-    {
-    }
+     public class Class10
+ {
+     public string Name { get; }
+     public string Salary { get; }
+     public Class11 class11 { get; }
+     
+     
+     public Class10(string name, string salary, Class11lass11)
+     {
+         this.Name = name;
+         this.Salary = salary;
+         this.class11 = clas11;
+     }
+ }
 }
