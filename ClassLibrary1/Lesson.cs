f25c36d031a010f2d6e26897f6002b07f7875fb1
Xtoto1911 <super.mr.kirill@mail.ru>
2023-03-29 20:03:27 +0300

    Фиксик- чинило

--- ClassLibrary1/Lesson.cs
+++ ClassLibrary1/Lesson.cs
@@ -9,14 +9,14 @@ namespace ClassLibrary1
     public class Lesson
     {
         public string Date { get; }
-        public Class3 Disciplina { get; }
+        public Discipline Disciplina { get; }
         public Class9 Sotrudnik { get; }
         public Class2 Auditoria { get; }
         public Group Group { get; }
         public Para Para { get; }
         public Class14 VidZanyatia { get; }
 
-        public Lesson(string date, Class3 disciplina, Class9 sotrudnik, Class2 auditoria, Group group, Para para, Class14 vidzanyatia)
+        public Lesson(string date, Discipline disciplina, Class9 sotrudnik, Class2 auditoria, Group group, Para para, Class14 vidzanyatia)
         {
             Date = date;
             Disciplina = disciplina;
