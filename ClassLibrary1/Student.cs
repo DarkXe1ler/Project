ada6f740a89e63695a239231f10df20f08944379
Ivdi_Qwerty <dimaivanov64101@gmail.com>
2023-03-31 10:56:40 +0300

    Исправлена дата рождения

--- ClassLibrary1/Student.cs
+++ ClassLibrary1/Student.cs
@@ -11,10 +11,10 @@ namespace ClassLibrary1
         public string Surname { get; }
         public string Name { get; }
         public string Middlename { get; }
-        public string DateOfBirth { get; }
+        public int DateOfBirth { get; }
         public Group Group{ get; }
 
-        public Student(string surname, string name, string middlename, string dateOfBirth, Group group)
+        public Student(string surname, string name, string middlename, int dateOfBirth, Group group)
         {
             Surname = surname;
             Name = name;
